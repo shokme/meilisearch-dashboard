07d5600993f074e07490391738350f41a63b13b3
Jordan <hello@jordan-massart.be>
2020-06-20 21:13:19 +0200

    add indexes page

---
+++ app/Http/Livewire/Dashboard/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public string $index;
+
+    public function mount($uid)
+    {
+        $this->index = $uid;
+    }
+
+    public function render()
+    {
+        return view('livewire.dashboard.index');
+    }
+}
--- resources/views/livewire/indexes.blade.php
+++ resources/views/livewire/indexes.blade.php
@@ -1,12 +1,15 @@
 <div>
-  <div class="mx-w-screen-xl mx-auto py-12 px-4 py-6 px-8 flex items-center justify-between">
+  <div class="mx-w-screen-xl mx-auto py-12 px-4 py-6 px-8 flex items-center justify-between" x-data="{ open: false }">
     <h2 class="font-extrabold tracking-tight text-primary-500 text-2xl leading-10">
       Indexes
     </h2>
     <div class="flex lg:flex-shrink-0 mt-0">
       <div class="inline-flex rounded shadow">
-        <button class="inline-flex items-center justify-center px-2 py-1 border border-transparent text-base leading-6 font-medium rounded text-white bg-primary-500 hover:bg-gray-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">Create Index</button>
+        <button @click="open = true" class="inline-flex items-center justify-center px-2 py-1 border border-transparent text-base leading-6 font-medium rounded text-white bg-primary-500 hover:bg-gray-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">Create Index</button>
       </div>
+    </div>
+    <div class="absolute" x-show="open">
+      <x-modal-centered-action :label="'Create Index'" />
     </div>
   </div>
 
@@ -25,22 +28,16 @@
           </tr>
           </thead>
           <tbody>
-          <tr class="border-b border-gray-200">
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-              <a href="#" class="text-blue-500">Estates</a>
-            </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-              1M 340k
-            </td>
-          </tr>
-          <tr>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-              <a href="#" class="text-blue-500">Projects</a>
-            </td>
-            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-              47k
-            </td>
-          </tr>
+          @foreach($indexes as $index)
+            <tr class="border-b border-gray-200">
+              <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
+                <a href="/dashboard/indexes/{{$index['uid']}}" class="text-blue-500">{{ $index['uid'] }}</a>
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                {{ number_format($index['numberOfDocuments'], 0, ',', ' ') }}
+              </td>
+            </tr>
+          @endforeach
           </tbody>
         </table>
       </div>
