61b44b8c404743f7be1cad8032951b648add1955
Jordan Massart <jordan.massart@ipmgroup.be>
2020-06-26 17:16:18 +0200

    add ranking

--- app/Http/Livewire/Dashboard/Rank.php
+++ app/Http/Livewire/Dashboard/Rank.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Dashboard;
 
 use App\Support\Facades\Meili;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use MeiliSearch\Client;
 use MeiliSearch\Exceptions\HTTPRequestException;
@@ -10,6 +11,8 @@ use MeiliSearch\Exceptions\HTTPRequestException;
 class Rank extends Component
 {
     public string $index;
+    public string $word = '';
+    public string $order = 'asc';
 
     /**
      * @return Client
@@ -19,9 +22,59 @@ class Rank extends Component
         return Meili::getIndex($this->index);
     }
 
-    public function rules()
+    public function get()
     {
         return $this->index()->getRankingRules();
+    }
+
+    public function add()
+    {
+        $list = $this->get();
+        $list = ["{$this->order}({$this->word})", ...$list];
+
+        $status = $this->index()->updateRankingRules($list);
+        $this->waitUpdate($status);
+    }
+
+    public function update($list)
+    {
+        dd($list);
+        $status = $this->index()->updateRankingRules($list);
+        $this->waitUpdate($status);
+    }
+
+    public function updateOrder($rule, $order)
+    {
+        $rules = collect($this->get())->map(function ($item) use ($rule, $order) {
+            if($item !== $rule) {
+                return $item;
+            }
+
+            if(Str::contains($rule, $order)) {
+                return $item;
+            } else {
+                $attribute = Str::between($rule, '(', ')');
+                $orderBy = Str::before($rule, $attribute);
+                return str_replace($orderBy, $order.'(', $rule);
+            }
+        })->all();
+
+        $status = $this->index()->updateRankingRules($rules);
+        $this->waitUpdate($status);
+    }
+
+    public function delete($id)
+    {
+        $all = $this->get();
+        unset($all[$id]);
+        $status = $this->index()->updateRankingRules(array_values($all));
+        $this->waitUpdate($status);
+    }
+
+    public function resetRankingRules()
+    {
+        $status = $this->index()->resetRankingRules();
+        $this->waitUpdate($status);
     }
 
     public function mount($uid)
@@ -31,7 +84,7 @@ class Rank extends Component
 
     public function render()
     {
-        return view('livewire.dashboard.rank', ['rules' => $this->rules()]);
+        return view('livewire.dashboard.rank', ['rules' => $this->get()]);
     }
 
     private function waitUpdate($id)
