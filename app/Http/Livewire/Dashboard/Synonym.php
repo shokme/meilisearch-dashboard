dd4c51d2296e1c2dd5b262d14e004ab4b2309588
Jordan Massart <hello@jordan-massart.be>
2020-07-04 16:24:07 +0200

    refator synonym to use trait

--- app/Http/Livewire/Dashboard/Synonym.php
+++ app/Http/Livewire/Dashboard/Synonym.php
@@ -2,24 +2,17 @@
 
 namespace App\Http\Livewire\Dashboard;
 
-use App\Support\Facades\Meili;
+use App\Support\MeilisearchTrait;
 use Livewire\Component;
-use MeiliSearch\Client;
 
 class Synonym extends Component
 {
+    use MeilisearchTrait;
+
     public string $index;
     public $updateSynonyms;
     public $alternative;
     public $type = 'synonyms';
-
-    /**
-     * @return Client
-     */
-    private function index()
-    {
-        return Meili::getIndex($this->index);
-    }
 
     public function get()
     {
@@ -72,13 +65,5 @@ class Synonym extends Component
     public function render()
     {
         return view('livewire.dashboard.synonym', ['synonyms' => $this->get()]);
-    }
-
-    private function waitUpdate($id)
-    {
-        while($this->index()->getUpdateStatus($id['updateId'])['status'] === 'enqueued') {
-            usleep(100 * 1000);
-            continue;
-        }
     }
 }
