d95e330eb55f938969c26c96599b8a3853a2e7a6
Jordan Massart <hello@jordan-massart.be>
2020-07-04 14:55:02 +0200

    refactor instance connection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use MeiliSearch\Client;
 
@@ -16,15 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('meili', function () {
+        $this->app->singleton('meili', function () {
             $db = DB::table('instances')->select('host', 'key')->where('active', 1)->first();
-            Log::debug('db', ['db' => $db]);
-            try {
-                return new Client($db->host, $db->key);
-            } catch (\Exception $e) {
-                report($e);
-                throw new \Exception('Could not connect to meilisearch');
-            }
+            return new Client($db->host, $db->key);
         });
     }
 
